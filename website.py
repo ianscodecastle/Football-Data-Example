83cabee6221193a143f991d67e27fa23ad5d6ce5
Ian Robinson <ianrobinson171@gmail.com>
2022-04-19 12:36:44 -0500

    images

--- website.py
+++ website.py
@@ -1,10 +1,13 @@
 import streamlit as st
-import players
+from urllib.request import urlopen
+from PIL import Image
 from Players import hurts, wentz, jackson
 import pandas as pd
 
 all = pd.concat([hurts.df_hurts, jackson.df_jackson, wentz.df_wentz], ignore_index=True, sort=False)
 
+header_image = Image.open(urlopen('https://i.pinimg.com/originals/93/b6/2e/93b62e4a2d6df775a6c198d6578f5a5f.png'))
+st.image(header_image)
 st.title('NFL QBs')
 
 st.subheader('Hurts')
