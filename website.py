c2fd4cf863a84213bacb213451d71ef301b8819d
Ian Robinson <ianrobinson171@gmail.com>
2022-04-19 12:04:58 -0500

    clean up code

--- website.py
+++ website.py
@@ -3,21 +3,18 @@ import players
 from Players import hurts, wentz, jackson
 import pandas as pd
 
-all = pd.concat([hurts.df_hurts, jackson.df_jackson, wentz.df_wentz])
+all = pd.concat([hurts.df_hurts, jackson.df_jackson, wentz.df_wentz], ignore_index=True, sort=False)
 
 st.title('NFL QBs')
-# st.subheader('Hurts')
-# st.table(players.get_hurts_passing_stats())
-# st.subheader('Jackson')
-# st.table(players.get_jackson_passing_stats())
-# st.subheader('Wentz')
-# st.table(players.get_wentz_passing_stats())
 
 st.subheader('Hurts')
 st.table(hurts.df_hurts)
+
 st.subheader('Jackson')
 st.table(jackson.df_jackson)
+
 st.subheader('Wentz')
 st.table(wentz.df_wentz)
+
 st.subheader('All Players')
 st.table(all)
